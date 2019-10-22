f0288168cfd632f61a3fc92721f65234a9cf0757
Cordell Charles <36606718+Cordell23@users.noreply.github.com>
2019-10-22 16:58:21 +0100

    Add files via upload

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/cordellcharles/Desktop/Dev/try_django/django_projects/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
