e8e57ac58c362b9487e186bb34a3e9a21bbe01bb
Cordell23 <cordell14@btinternet.com>
2019-11-26 16:13:55 +0000

    updating views from function based views to class based views

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,18 +1,20 @@
 from django.urls import path
 from .views import (
-    article_detail_view,
-    ArticleList,
-    article_create_view,
-    article_delete_view,
+    ArticleDetailView,
+    ArticleListView,
+    ArticleCreateView,
+    ArticleEditView,
+    ArticleDeleteView,
     dynamic_lookup_view,
     )
 
 
 app_name = 'blog'
 urlpatterns = [
-    path('<int:my_id>/', dynamic_lookup_view, name= 'article-detail'),
-    path('', ArticleList.as_view(), name= 'article-list'),
-    path('create', article_create_view, name= 'article-create'),
-    path('delete/<int:my_id>/', article_delete_view, name= 'article-delete')
+    path('<int:my_id>/', ArticleDetailView.as_view(), name= 'article-detail'),
+    path('', ArticleListView.as_view(), name= 'article-list'),
+    path('create', ArticleCreateView.as_view(), name= 'article-create'),
+    path('edit/<int:my_id>/', ArticleEditView.as_view(), name= 'article-edit'),
+    path('delete/<int:my_id>/', ArticleDeleteView.as_view(), name= 'article-delete')
 ]
 
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView
+from django.urls import reverse
+from django.views.generic import (
+	ListView,
+	DetailView,
+	CreateView,
+	UpdateView,
+	DeleteView
+)
+
 from .models import Article
 from .forms import ArticleForm
 
 # Create your views here.
 
+class ArticleDetailView(DetailView):
+	template_name = 'articles/article_detail.html'
+
+	def get_object(self):
+		id_ = self.kwargs.get("my_id")
+		return get_object_or_404(Article, id=id_)
+
+
+
+''' Function based detail view
 def article_detail_view(request):
 
 	obj = Article.objects.get()
@@ -17,6 +35,7 @@ def article_detail_view(request):
 		'object' : obj
 	}
 	return render(request, "articles/article_detail.html", context)
+'''
 
 
 
@@ -30,7 +49,19 @@ def dynamic_lookup_view(request, my_id): # Dynamic Url Routing
 
 
 
-'''
+
+
+
+
+
+class ArticleListView(ListView): # Class based list view
+	# model = Article
+	template_name = 'articles/article_list.html'
+	queryset = Article.objects.all()  # blog/<modelname>_list.html
+
+
+''' Function based list view 
+
 def article_list_view(request):
  
 	queryset = Article.objects.all() # gives back list of objects
@@ -40,13 +71,20 @@ def article_list_view(request):
 	return render(request, "articles/article_list.html", context)
 '''
 
-class ArticleList(ListView):
-	model = Article
 
 
 
-def article_create_view(request): # Function based views
 
+
+
+class ArticleCreateView(CreateView): # Class based create view
+	# model = Article
+	template_name = 'articles/article_create.html'
+	form_class = ArticleForm
+	queryset = Article.objects.all()
+
+''' # Function based create view
+def article_create_view(request):
 	form = ArticleForm(request.POST or None)
 
 	if form.is_valid():
@@ -60,9 +98,53 @@ def article_create_view(request): # Function based views
 		'form' : form
 	}
 	return render(request, "articles/article_create.html", context)
+'''
 
 
 
+class ArticleEditView(UpdateView): # Class based editing view
+	
+	template_name = 'articles/article_create.html'
+	form_class = ArticleForm
+
+	def get_object(self):
+		id_ = self.kwargs.get("my_id")
+		return get_object_or_404(Article, id=id_)
+
+	def form_valid(self, form):
+		return super().form_valid(form)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class ArticleDeleteView(DeleteView): # Class based editing view
+	
+	template_name = 'articles/article_delete.html'
+	# form_class = ArticleForm
+
+	def get_object(self):
+		id_ = self.kwargs.get("my_id")
+		obj = get_object_or_404(Article, id=id_)
+		return obj
+
+	def get_success_url(self):
+		return reverse('blog:article-list')
+
+
+'''
+ # Function based delete view
 def article_delete_view(request, my_id):
 	obj = get_object_or_404(Article, id= my_id)
 	# POST request
@@ -76,4 +158,4 @@ def article_delete_view(request, my_id):
 		"object": obj
 	}
 	return render(request, "articles/article_delete.html", context)
-
+'''
